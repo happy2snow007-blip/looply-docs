083db2968338058d54dcb548e7d0d460edf0106a
happy2snow007 <happy2snow007@gmail.com>
2026-06-10 18:52:47 +0800

    sync: 文档更新 2026-06-10 18:52

--- sync.py
+++ sync.py
@@ -116,7 +116,7 @@ MODULES = {
         'config_key': 'pdp',
         'artifacts': {
             'prototype': {
-                'subdir': 'UI',
+                'subdir': '原型',
                 'pattern': r'looply-商详页CMS配置后台原型\.html',
                 'no_version': True,
             },
