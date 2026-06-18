4d9a8a0c6dd29dd40992970b22dd65b24da18bd3
happy2snow007 <happy2snow007@gmail.com>
2026-06-18 16:37:59 +0800

    sync: 文档更新 2026-06-18 16:37

--- prototype-config.js
+++ prototype-config.js
@@ -11,9 +11,9 @@ var PROTOTYPE_CONFIG = {
   'exchange':     'docs-汇率管理/原型/looply-汇率管理后台原型-v8.3-antd.html',
   'user':         'docs-用户管理/原型/looply-用户管理后台原型-v2-antd.html',
   'pdp':          'docs-商详/原型/looply-商详页CMS配置后台原型-v3-antd.html',
+  'cms':          'docs-CMS管理/原型/looply-cms-antd-原型-v1.0.html',
   'logistics':    'docs-物流管理/原型/looply-物流管理后台原型-v5.10-antd.html',
   'social_share': 'docs-社媒分享管理/原型/looply-社媒分享管理-antd-原型-v1.2.html',
   'subscription': 'docs-订阅管理/原型/looply-订阅管理-antd-原型-v2.html',
   '地址库管理':        'docs-地址库管理/原型/looply-地址库管理后台原型-v5-antd.html',
-  'cms':         'docs-CMS管理/原型/looply-cms-antd-原型-v1.0.html',
 };
