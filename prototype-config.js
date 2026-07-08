f62130fbd7c53ffd08c9cee2f677bcdf5db672a7
zz <zhuowu78@gmail.com>
2026-07-08 11:40:58 +0800

    sync: 文档更新 2026-07-08 11:40

--- prototype-config.js
+++ prototype-config.js
@@ -4,13 +4,14 @@
  * 此文件由 sync.py 自动生成，请勿手动修改。
  */
 var PROTOTYPE_CONFIG = {
-  'product':     'docs-商品系统/原型/looply-商品管理后台原型-v40-antd.html',
-  'market':      'docs-market系统/原型/looply-market后台原型-v21-antd.html',
-  'inventory':   'docs-库存管理/原型/looply-库存管理后台原型-v8-antd.html',
-  'translation': 'docs-翻译管理/原型/looply-翻译管理后台原型-v8.html',
-  'exchange':    'docs-汇率管理/原型/looply-汇率管理后台原型-v8.3-antd.html',
-  'user':        'docs-用户管理/原型/looply-用户管理后台原型-v2-antd.html',
-  'pdp':         'docs-商详/原型/looply-商详页CMS配置后台原型-v3-antd.html',
-  'collection':  'docs-Collection管理/原型/looply-类目管理-后台-v0.19-antd.html',
-  'shop':        'docs-shop页/后台原型/looply-shop页导航栏配置-后台原型-v0.1-antd.html',
+  'product':      'docs-商品系统/原型/looply-商品管理后台原型-v40-antd.html',
+  'market':       'docs-market系统/原型/looply-market后台原型-v21-antd.html',
+  'inventory':    'docs-库存管理/原型/looply-库存管理后台原型-v8-antd.html',
+  'translation':  'docs-翻译管理/原型/looply-翻译管理后台原型-v8.html',
+  'exchange':     'docs-汇率管理/原型/looply-汇率管理后台原型-v8.3-antd.html',
+  'user':         'docs-用户管理/原型/looply-用户管理后台原型-v2-antd.html',
+  'pdp':          'docs-商详/原型/looply-商详页CMS配置后台原型-v3-antd.html',
+  'collection':   'docs-Collection管理/原型/looply-类目管理-后台-v0.19-antd.html',
+  'shop':         'docs-shop页/后台原型/looply-shop页导航栏配置-后台原型-v0.1-antd.html',
+  'social_share': 'docs-社媒分享管理/原型/looply-社媒分享管理-antd-原型-v1.2.html',
 };
